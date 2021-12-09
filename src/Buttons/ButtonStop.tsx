21a4bc2fe87966cc6a3681607f1099b78d450e0f
Ivan Ilichev <swggnuko.dev@gmail.com>
2021-12-09 16:36:06 +0200

    Init

---
+++ src/Buttons/ButtonStop.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ButtonStop() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/Tasks/TaskActive.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TaskActive() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
