2d773e0c89477935afd59484c796de6661fff35b
steezy2401 <swggnuko.dev@gmail.com>
2021-12-27 00:16:42 +0200

    Full task functionality

--- src/components/Modals/ModalTaskFull.tsx
+++ src/components/Modals/ModalTaskFull.tsx
@@ -1,19 +1,15 @@
 import NiceModal, { useModal } from "@ebay/nice-modal-react";
+import { useEffect, useState } from "react";
 
+//Components
 import Deadline from "../Deadline/Deadline";
 import DifficultyBadge from "../DifficultyBadge/DifficultyBadge";
-import { Difficulties } from "../../types/TypesDifficulties";
 import ButtonComplete from "../Buttons/ButtonComplete";
 
-interface TaskData {
-  title: string;
-  goalTitle: string;
-  text: string;
-  deadline: string;
-  difficulty: Difficulties;
-  isActive: boolean;
-}
+//Types
+import { TaskData } from "../../types/TypesTask";
 
+//! Mock data
 const data: TaskData = {
   title: "Watch a youtube video",
   goalTitle: "Learn Javascript",
@@ -25,6 +21,24 @@ const data: TaskData = {
 
 export default NiceModal.create(({ taskId }) => {
   const modal = useModal();
+
+  const [isBusy, setIsBusy] = useState(true);
+  const [taskData, setTaskData] = useState<TaskData>({} as TaskData);
+
+  useEffect(() => {
+    //TODO: Call an API
+
+    //! Mock data
+    setTaskData(data);
+    setIsBusy(false);
+  }, []);
+
+  const handleComplete = () => {
+    //TODO: Save to the database
+
+    console.log(taskId)
+  }
+
   return (
     <div
       className={`min-w-screen h-screen fixed left-0 top-0 flex justify-center items-center inset-0 z-50 animate-fade-in`}
@@ -33,24 +47,28 @@ export default NiceModal.create(({ taskId }) => {
         className="absolute bg-black opacity-80 inset-0 z-0 "
         onClick={modal.remove}
       ></div>
-      <div className="w-full max-w-lg p-1 px-2 relative mx-auto my-auto rounded-lg shadow-lg bg-white animate-fade-in-up">
-        <div className="flex flex-col mb-1">
-          <span className="text-grey-darker font-medium text-sm">
-            {data.goalTitle}
-          </span>
-          <span className="font-medium text-black text-3xl">{data.title}</span>
+      {!isBusy && (
+        <div className="w-full max-w-lg p-1 px-2 relative mx-auto my-auto rounded-lg shadow-lg bg-white animate-fade-in-up">
+          <div className="flex flex-col mb-1">
+            <span className="text-grey-darker font-medium text-sm">
+              {taskData.goalTitle}
+            </span>
+            <span className="font-medium text-black text-3xl">
+              {taskData.title}
+            </span>
+          </div>
+          <div className="flex row gap-2">
+            <Deadline time={taskData.deadline} />
+            <DifficultyBadge difficulty={taskData.difficulty} />
+          </div>
+          <div className="text-lg mt-5 text-black">
+            <p className="mb-3">{taskData.text}</p>
+          </div>
+          <div className="mt-20 mb-1 grid place-items-end">
+            <ButtonComplete action={handleComplete}/>
+          </div>
         </div>
-        <div className="flex row gap-2">
-          <Deadline time={data.deadline} />
-          <DifficultyBadge difficulty={data.difficulty} />
-        </div>
-        <div className="text-lg mt-5 text-black">
-          <p className="mb-3">{data.text}</p>
-        </div>
-        <div className="mt-20 mb-1 grid place-items-end">
-          <ButtonComplete />
-        </div>
-      </div>
+      )}
     </div>
   );
 });
